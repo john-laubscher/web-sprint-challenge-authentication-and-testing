a448d4637c3149faf0a62adc969ee8d2c1e6b06e
John Laubscher <jlmemphiscommanders@gmail.com>
2021-07-23 13:07:50 -0500

    debugging

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,5 +1,18 @@
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../../secrets/secrets");
+
 module.exports = (req, res, next) => {
-  next();
+  const token = req.headers.authorization;
+  if (!token) {
+    return next({ status: 401, message: "token required" });
+  }
+  jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+    if (err) {
+      return next({ status: 401, message: "token invalid" });
+    }
+    req.decodedJwt = decodedToken;
+    next();
+  });
   /*
     IMPLEMENT
 
